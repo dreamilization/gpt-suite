a5e37a1aeb8b3082ab749849d53d4513bd33b500
dreamilization <40374124+dreamilization@users.noreply.github.com>
2024-04-05 10:53:20 -0400

    add documentation for GPTMPHandler, better handling api_key

--- src/gpt_suite/gpt_mp_handler.py
+++ src/gpt_suite/gpt_mp_handler.py
@@ -1,18 +1,34 @@
 from tqdm.contrib.concurrent import process_map
 from typing import List, Dict
 from warnings import warn
-from os import cpu_count
+from os import cpu_count, environ
 from . import gpt_util
 import inspect
 
 
 class GPTMPHandler:
-    def __init__(self, api_key: str,
+    def __init__(self, api_key: str = environ.get('OPENAI_API_KEY'),
                  num_worker: int = min(32, cpu_count() + 4),
                  gen_conf: dict = None,
                  max_retries: int = 2,
                  **kwargs):
+        """
+        Initialize the GPT Multi-Processing Handler
+        :param api_key: OpenAI API Key as a string, defaults to environment variable OPENAI_API_KEY
+        :type api_key: str(, optional)
+        :param num_worker: Number of worker threads to use, defaults to CPU count + 4 or 32, whichever is smaller
+        :type num_worker: int(, optional)
+        :param gen_conf: Generation parameters (e.g. top_p, presence_penalty, etc.). Possible keys can be found at
+         https://platform.openai.com/docs/api-reference/chat/create, defaults to gpt_util._generation_config if not set
+        :type gen_conf: dict(, optional)
+        :param max_retries: When processing batch, how many times to retry failed instances, defaults to 2
+        :type max_retries: int(, optional)
+        :param kwargs: Additional arguments to pass to OpenAI when initializing the OpenAI client
+        :raises AssertionError: If the OpenAI API Key is not provided
+        """
         # Initialize backend utility
+        assert api_key, ("OpenAI API Key is required. "
+                         "Either pass as argument or set as environment variable OPENAI_API_KEY")
         self.api_key = api_key
         self.openai_args = kwargs
         self.gen_conf = gen_conf
@@ -28,7 +44,13 @@ class GPTMPHandler:
             [arg for arg in func_arg_list if func_signature.parameters[arg].default is inspect.Parameter.empty]
         self.func_args: Dict[str, type] = {arg: func_signature.parameters[arg].annotation for arg in func_arg_list}
 
-    def dict_verifier(self, input_dict: dict) -> bool:
+    def _dict_verifier(self, input_dict: dict) -> bool:
+        """
+        Verify if the input dictionary is valid for processing
+        :param input_dict: Individual instance that will be processed when process() is called
+        :type input_dict: dict
+        :return: True if the dictionary is valid, False otherwise
+        """
         # Check if all required arguments are provided
         for k in self.func_required_arg_list:
             if k not in input_dict:
@@ -44,7 +66,15 @@ class GPTMPHandler:
         return True
 
     def add_batch(self, batch: List[dict]):
-        verification_result: List[bool] = process_map(self.dict_verifier,
+        """
+        Add a batch of instances to the queue, the batch will be processed when process() is called.
+        Each added batch will be verified to ensure all required arguments are provided and of correct type.
+        :param batch: A list of dictionaries, each dictionary should contain all required arguments used in
+        gpt_util.generate_explanation. If not an error will be raised.
+        :type batch: List[dict]
+        :raises AssertionError: If any dictionary in the batch has missing or invalid arguments
+        """
+        verification_result: List[bool] = process_map(self._dict_verifier,
                                                       batch,
                                                       max_workers=self.num_worker,
                                                       chunksize=max(1, len(batch) // self.num_worker),
@@ -61,6 +91,18 @@ class GPTMPHandler:
             self.queue.append(b)
 
     def process(self, rerun_on_error: bool = False) -> List[Dict[str, str]]:
+        """
+        Process the batch of instances added to the queue. The function will return a list of dictionaries, each
+        dictionary contains the output of gpt_util.generate_explanation for each instance in the batch. If
+        rerun_on_error is set to True, the function will retry failed instances up to self.max_retries number of times.
+        Any instance that failed after self.max_retries will be returned as an empty dictionary.
+        :param rerun_on_error: If set to True, the function will retry failed instances up to self.max_retries number of
+        times, defaults to False
+        :type rerun_on_error: bool
+        :return: list of dictionaries, each dictionary contains the output of gpt_util.generate_explanation for each
+        instance in the batch
+        :rtype: List[Dict[str, str]]
+        """
         # copy the queue to a local variable and clear the public facing queue
         queue = self.queue[:]
         self.queue = []
