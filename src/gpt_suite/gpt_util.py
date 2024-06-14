36766966d15528bff2746b32a8239d70531b171e
dreamilization <40374124+dreamilization@users.noreply.github.com>
2024-06-14 13:10:37 -0400

    docs: add documentation for gpt_util.py

--- src/gpt_suite/gpt_util.py
+++ src/gpt_suite/gpt_util.py
@@ -20,6 +20,14 @@ __version__: str = '0.2.2'
 
 
 def _version_checker(target_majors: list) -> bool:
+    """
+    Check if the current version of OpenAI is compatible with the target major versions
+    :param target_majors: List of target major versions
+    :type target_majors: list
+
+    :return: True if OpenAI version is larger or equal to the minimal version, False otherwise
+    :rtype: bool
+    """
     minimal_version = min(target_majors)
     # Check if compatible with openai package
     major = int(openai.__version__.split('.')[0])
@@ -33,6 +41,27 @@ def _get_response(message: list,
                   debug_log_path: str = None,
                   client: OpenAI = _client,
                   **kwargs) -> ChatCompletion:
+    """
+    Wrapper function to get response from OpenAI
+    :param message: List of messages in the format defined in OpenAI API
+    :type message: list
+
+    :param model_name: Name for the OpenAI model to use, defaults to "gpt-3.5-turbo"
+    :type model_name: str(, optional)
+
+    :param debug_log_path: Path to save the debug log, defaults to None
+    :type debug_log_path: str(, optional)
+
+    :param client: OpenAI client object, defaults to `_client` if not provided
+    :type client: OpenAI(, optional)
+
+    :param kwargs: Generation parameters (e.g. top_p, presence_penalty, etc.). Possible keys can be found at
+        https://platform.openai.com/docs/api-reference/chat/create, defaults to _generation_config if not set
+    :type kwargs: dict(, optional)
+
+    :return: ChatCompletion object from OpenAI
+    :rtype: ChatCompletion
+    """
     # Check is the utility is properly setup
     assert client is not None, "need to run init() first or provide client."
     # Prepare generation arguments
@@ -57,17 +86,45 @@ def _get_response(message: list,
 
 
 def _append_question(context: list, question: str):
-    # Append question to the existing dictionary
+    """
+    Append a question to the context list
+
+    :param context: List where the question will be appended
+    :type context: list
+
+    :param question: Question to append
+    :type question: str
+
+    :return: None
+    """
     context.append({"role": "user", "content": question})
 
 
 def _append_response(context: list, response: str):
-    # Append responds to the existing dictionary
+    """
+    Append a response from GPT to the context list
+
+    :param context: List where the response will be appended
+    :type context: list
+
+    :param response: Response to append
+    :type response: str
+
+    :return: None
+    """
     context.append({"role": "assistant", "content": response})
 
 
 def _extract_raw_result(raw: ChatCompletion) -> str:
-    # Return the striped text from the completion
+    """
+    Extract the response from the ChatCompletion object
+
+    :param raw: ChatCompletion object from OpenAI, contains the response
+    :type raw: ChatCompletion
+
+    :return: Extracted response from the ChatCompletion object
+    :rtype: str
+    """
     return raw.choices[0].message.content.strip()
 
 
@@ -79,6 +136,37 @@ def generate_explanation(init_context: str,
                          debug_log: str = None,
                          client: OpenAI = _client,
                          **kwargs) -> Dict[str, str]:
+    """
+    Given a list of questions for a given context, generate responses for each question step by step.
+    :param init_context: Part of the first question, will be include as `initial_context` in the return dict
+    :type init_context: str
+
+    :param questions: List of questions to ask
+    :type questions: list
+
+    :param model_name: Name for the OpenAI model to use, defaults to "gpt-3.5-turbo"
+    :type model_name: str(, optional)
+
+    :param verbose: Print chat history if set to True, defaults to False
+    :type verbose: bool(, optional)
+
+    :param task_desc: System prompt, defaults to None
+    :type task_desc: str(, optional)
+
+    :param debug_log: Path to save the debug log, defaults to None
+    :type debug_log: str(, optional)
+
+    :param client: OpenAI client object, defaults to `_client` if not provided
+    :type client: OpenAI(, optional)
+
+    :param kwargs: Generation parameters (e.g. top_p, presence_penalty, etc.). Possible keys can be found at
+        https://platform.openai.com/docs/api-reference/chat/create, defaults to _generation_config if not set
+    :type kwargs: dict(, optional)
+
+    :return: Dictionary with responses for each question. Each key is a question and the value is the response to the
+        question.
+    :rtype: dict
+    """
     # Check is the utility is properly setup
     assert client is not None, "need to run init() first or provide client."
 
@@ -116,6 +204,16 @@ def generate_explanation(init_context: str,
 
 
 def generate_explanation_wrapper(arg_dict: dict) -> Dict[str, str]:
+    """
+    Wrapper function of `generate_explanation` for `GPTMPHandler` to use in multiprocessing.
+
+    :param arg_dict: Argument dictionary for `generate_explanation`
+    :type arg_dict: dict
+
+    :return: Dictionary with responses for each question. Each key is a question and the value is the response to the
+        question.
+    :rtype: dict
+    """
     # Initialize OpenAI Object
     client = OpenAI(**arg_dict['openai_args'])
     del arg_dict['openai_args']
@@ -128,6 +226,19 @@ def generate_explanation_wrapper(arg_dict: dict) -> Dict[str, str]:
 
 
 def init(api_key: str, gen_conf: dict = None, **kwargs):
+    """
+    Initialize the OpenAI utility with the given API key and generation configuration.
+    :param api_key: OpenAI API Key
+    :type api_key: str
+
+    :param gen_conf: Generation configuration to override the default configuration, defaults to None
+    :type gen_conf: dict(, optional)
+
+    :param kwargs: Other arguments to pass to OpenAI client, defaults to None
+    :type kwargs: dict
+
+    :return: None
+    """
     global _generation_config
     global _client
 
